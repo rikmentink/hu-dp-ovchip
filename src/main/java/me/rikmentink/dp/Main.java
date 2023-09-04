4eef2a61678ffd7701d66259130f6dbb5a3de4e2
Rik Mentink <rik.mentink@icloud.com>
2023-09-04 16:25:50 +0200

    Opdracht P2 verder gemaakt.
    Alleen de testfunctie moet nog geimplementeerd worden.

--- src/main/java/me/rikmentink/dp/Main.java
+++ src/main/java/me/rikmentink/dp/Main.java
@@ -2,40 +2,31 @@ package me.rikmentink.dp;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 public class Main {
     private static String DATABASE_URL = "jdbc:postgresql://localhost/hu-dp";
+    private static Connection conn;
 
-    public static void main(String[] args) {
-        try {
-            Connection conn = getConnection();
-            ResultSet users = getAllUsers(conn);
-
-            System.out.println("Alle reizigers:");
-            int count = 1;
-            while (users.next()) {
-                System.out.println(
-                        String.format("  #%s: %s (%s)", count, users.getString("name"), users.getString("birthdate")));   
-                count++;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws SQLException {
+        conn = getConnection();
+        // TODO: Roep test functie aan.
+        closeConnection();
     }
 
     private static Connection getConnection() throws SQLException {
         Properties props = new Properties();
         props.setProperty("user", "rikmentink");
-        props.setProperty("password", "test");
+        props.setProperty("password", "wachtwoord");
         return DriverManager.getConnection(DATABASE_URL, props);
     }
 
-    private static ResultSet getAllUsers(Connection conn) throws SQLException {
-        Statement stmt = conn.createStatement();
-        return stmt.executeQuery("SELECT * FROM users");
+    private static void closeConnection() throws SQLException {
+        if (conn != null) {
+            conn.close();
+        }
     }
+
+    // TODO: Implementeer test functie van Canvas
 }
--- src/main/java/me/rikmentink/dp/models/ReizigerDAOPsql.java
+++ src/main/java/me/rikmentink/dp/models/ReizigerDAOPsql.java
@@ -1,11 +1,107 @@
 package me.rikmentink.dp.models;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ReizigerDAOPsql {
+public class ReizigerDAOPsql implements ReizigerDAO {
     private Connection conn;
 
     public ReizigerDAOPsql(Connection conn) {
         this.conn = conn;
+    }
+
+    @Override
+    public boolean save(Reiziger reiziger) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO reiziger VALUES (?,?,?,?,?)");
+        stmt.setInt(1, reiziger.getId());
+        stmt.setString(2, reiziger.getVoorletters());
+        stmt.setString(3, reiziger.getTussenvoegsel());
+        stmt.setString(4, reiziger.getAchternaam());
+        stmt.setString(5, reiziger.getGeboortedatum().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        return stmt.execute();
+    }
+
+    @Override
+    public boolean update(Reiziger reiziger) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE"); // TODO: Juiste update statement maken.
+        stmt.setInt(1, reiziger.getId());
+        stmt.setString(2, reiziger.getVoorletters());
+        stmt.setString(3, reiziger.getTussenvoegsel());
+        stmt.setString(4, reiziger.getAchternaam());
+        stmt.setObject(5, reiziger.getGeboortedatum());
+        return stmt.executeUpdate() > 0;
+    }
+
+    @Override
+    public boolean delete(Reiziger reiziger) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM reiziger WHERE id = ?");
+        stmt.setInt(1, reiziger.getId());
+        return stmt.execute();
+    }
+
+    @Override
+    public List<Reiziger> findById(int id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM reiziger WHERE id = ?");
+        stmt.setInt(1, id);
+        ResultSet rs = stmt.executeQuery();
+
+        List<Reiziger> reizigers = new ArrayList<Reiziger>();
+        while (rs.next()) {
+            Reiziger reiziger = new Reiziger(
+                rs.getInt("reiziger_id"),
+                rs.getString("voorletters"),
+                rs.getString("tussenvoegsel"),
+                rs.getString("achternaam"),
+                LocalDate.parse(rs.getString("geboortedatum"), DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+            );
+            reizigers.add(reiziger);
+        }
+        return reizigers;
+    }
+
+    @Override
+    public List<Reiziger> findByGbdatum(String datum) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM reiziger WHERE geboortedatum = ?");
+        stmt.setString(1, datum);
+        ResultSet rs = stmt.executeQuery();
+
+        List<Reiziger> reizigers = new ArrayList<Reiziger>();
+        while (rs.next()) {
+            Reiziger reiziger = new Reiziger(
+                rs.getInt("reiziger_id"),
+                rs.getString("voorletters"),
+                rs.getString("tussenvoegsel"),
+                rs.getString("achternaam"),
+                LocalDate.parse(rs.getString("geboortedatum"), DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+            );
+            reizigers.add(reiziger);
+        }
+        return reizigers;
+
+    }
+
+    @Override
+    public List<Reiziger> findAll() throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM reiziger");
+        ResultSet rs = stmt.executeQuery();
+
+        List<Reiziger> reizigers = new ArrayList<Reiziger>();
+        while (rs.next()) {
+            Reiziger reiziger = new Reiziger(
+                rs.getInt("reiziger_id"),
+                rs.getString("voorletters"),
+                rs.getString("tussenvoegsel"),
+                rs.getString("achternaam"),
+                LocalDate.parse(rs.getString("geboortedatum"), DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+            );
+            reizigers.add(reiziger);
+        }
+        return reizigers;
     }
 }
